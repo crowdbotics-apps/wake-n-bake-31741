d440529ca3ec2f23101eb6f5b3ccb4bc68dd805f
Crowdbotics <team@crowdbotics.com>
2021-10-28 09:16:32 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "wake_n_bake_31741",
+  url: "https://wake_n_bake_31741.botics.co",
+  api: "https://wake_n_bake_31741.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
